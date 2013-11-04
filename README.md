3b0023d323fcc85c6e3bb3b20fd0a5374bbb5b16
Hayal <just4u_ly@yahoo.com>
2013-11-04 16:59:26 +0700

    readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 revo
-====
+===
+
+
+ksajdkasjkdjask=
--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "HEllo World";?>
+<?php 
+$start= 0;
+$end=2000;
+
+for($i=$start;$i<$end;$i++){
+	echo $i . PHP_EOL;
+	sleep(1);
+}
+
+?>
